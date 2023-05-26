170bdecc86118c3b46ef01836f0991d1bd66288b
Ashima Ashima <asashima@sheridancollege.ca>
2023-05-26 08:29:44 -0400

    Added time

--- src/arraydemos23friam/Student.java
+++ src/arraydemos23friam/Student.java
@@ -3,7 +3,7 @@ package arraydemos23friam;
 
 
 public class Student {
-    //comment added at 9:27
+    //comment added at 8:29
     private String sId;
     private String sName;
     private String address;
